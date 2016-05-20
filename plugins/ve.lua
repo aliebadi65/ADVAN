356ad2e954f9140d64c19811b89c9812a519f99f
aliebadi65 <aliebadi66@gmail.com>
2016-05-20 14:53:53 +0430

    Update ve.lua

--- plugins/ve.lua
+++ plugins/ve.lua
@@ -3,12 +3,12 @@ do
 local function run(msg, matches)
   if matches[1]:lower() == 'version' then -- Versoin of TeleAdvan: V3!
      send_document(get_receiver(msg), "./data/advan_v3.webp", ok_cb, false)
-    return 'Version of TeleAdvan: V3!'
+    return 'Version of Smart: V3.7!'
   end
 end
 return {
   patterns = {
-    "^!([Vv][Ee][Rr][Ss][Ii][Oo][Nn])$"
+    "^!([Vv][Ee][Rr])$"
   }, 
   run = run 
 }
